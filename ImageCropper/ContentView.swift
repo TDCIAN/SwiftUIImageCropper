fd145618ca8da358cfbe7e4bcbaa4430cc9bbc56
Jeongmin <tdcian71@gmail.com>
2021-05-23 14:23:04 +0900

    Initial Commit

---
+++ ImageCropper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageCropper
+//
+//  Created by APPLE on 2021/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageCropper/ImageCropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCropperApp.swift
+//  ImageCropper
+//
+//  Created by APPLE on 2021/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageCropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
